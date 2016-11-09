d1b9c3056462456fd8aa54e666384515d9229d3e
朱云峰 <zhuyunfeng@zafh.com.cn>
2016-11-09 14:40:38 +0800

    commit for log4j

--- src/test/java/com/cgw360/cls/dubbo/api/cardealer/ICarAccountService/CarAccountsStepdefs.java
+++ src/test/java/com/cgw360/cls/dubbo/api/cardealer/ICarAccountService/CarAccountsStepdefs.java
@@ -125,6 +125,15 @@ public class CarAccountsStepdefs {
 	}
 	
 	
-	
+	public static void main(String[] args) {
+		
+
+        // 记录debug级别的信息  
+        LOGGER.debug("This is debug message.");  
+        // 记录info级别的信息  
+        LOGGER.info("This is info message.");  
+        // 记录error级别的信息  
+        LOGGER.error("This is error message.");  
+	}
 	
 }
